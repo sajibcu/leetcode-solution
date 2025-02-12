6b19e1431b50a28b29bf725f9f0c702dca518408
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-12 23:54:12 +0800

    Time: 511 ms (78.42%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,10 @@
 -- # Write your MySQL query statement below
 select e.name
 from Employee e
-where e.id in ( 
+inner join  ( 
     select ee.managerId from Employee ee 
     where ee.managerId is not null and ee.managerId <> ee.id 
     group by ee.managerId
     having (count(1) > 4)
-)
+) m on m.managerId = e.id
 
