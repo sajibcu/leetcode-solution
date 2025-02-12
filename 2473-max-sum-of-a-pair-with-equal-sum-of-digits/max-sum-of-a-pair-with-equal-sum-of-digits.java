ceaa12ce6dd64f209a3723c4a828e7ec66c1aa6c
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-12 10:14:16 +0800

    Time: 40 ms (72.41%) | Memory: 59.8 MB (58.52%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,52 +1,26 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        int[] frequencyOfDigitSum = new int[100];
-        Map<Integer,Integer[]> originalNumber  = new HashMap<>();
-
-        for(int n: nums) {
-            int sum = getDigitSum(n);
-            frequencyOfDigitSum[sum]++;
-            Integer[] original = originalNumber.get(sum);
-            if(original == null) {
-                original = new Integer[2];
-            }
-            setOriginal(original,n);
-            originalNumber.put(sum, original );
-        }
+    
+        Map<Integer,int[]> originalNumber  = new HashMap<>();
 
         int maxSum = -1;
 
-        for(int i=0;i<100;i++){
-            if(frequencyOfDigitSum[i] > 1) {
-                Integer[] original = originalNumber.get(i);
+        for(int n: nums) {
+            int sum = getDigitSum(n);
+            originalNumber.putIfAbsent(sum, new int[]{-1, -1});
+            int[] original = originalNumber.get(sum);
+            if(original[0] < n ){
+                original[1] = original[0];
+                original[0] = n;
+            }else if( original[1] < n )
+                original[1] = n;
+
+            if(original[1] > -1)
                 maxSum = Math.max(maxSum, original[0] + original[1]);
-            }
         }
+
         return maxSum;
     }
-    public void setOriginal(Integer[] nums, int value) {
-        if(nums[0] == null) {
-            nums[0] = value;
-            return;
-        }
-
-        if(nums[1] == null) {
-            nums[1] = value;
-            return;
-        }
-
-        if(nums[0] < value) {
-            int temp = nums[0];
-            nums[0] = value;
-            if(nums[1] < temp )
-            nums[1] = temp;
-            return;
-        }
-
-        if( nums[1]< value)
-        nums[1] = value;
-    }
-
 
     public int getDigitSum(int num) {
         int sum = 0;
