c7e49e08d05582647d6bc188c137c6c040c54fb8
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-12 09:22:49 +0800

    Time: 1 ms (99.46%) | Memory: 42.1 MB (84.46%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+
+        StringBuilder sb = new StringBuilder(s);
+
+        int index = sb.indexOf(part);
+
+        while(index != -1) {
+            sb.replace(index, index+part.length(), "");
+            index = sb.indexOf(part);
+        }
+
+        return sb.toString();
+    }
+}
