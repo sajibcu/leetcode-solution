794c4e39160d48315e7e87a0e478b5655fdf6dbe
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-15 10:00:58 +0800

    Time: 33 ms (66.77%) | Memory: 43.9 MB (72.73%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int punishmentNumber(int n) {
+        int ans = 0;
+        while( n > 0 ) {
+            if(isPunishmentNumber(n,Integer.toString(n*n), 0, 0,0)) {
+                ans +=n*n;
+            }
+            n--;
+        }
+
+        return ans;
+    }
+
+    public boolean isPunishmentNumber(int n, String s, int index, int totalSum, int currentSum) {
+        // if( currenSum > n ) return false;
+        if(index >= s.length() ) return n ==(totalSum+currentSum);
+        int a = s.charAt(index)-'0';
+        return isPunishmentNumber(n,s,index+1, totalSum, currentSum*10+a )
+        || isPunishmentNumber(n,s,index+1, totalSum + currentSum, a );
+    }
+}
