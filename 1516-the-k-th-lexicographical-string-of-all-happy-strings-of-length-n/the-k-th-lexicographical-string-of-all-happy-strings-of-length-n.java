2bd48dfb197927b1ae2ef1780a5f836b3f198ab7
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-19 15:19:27 +0800

    Time: 1 ms (99.09%) | Memory: 41.1 MB (94.08%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,29 +1,28 @@
 class Solution {
     private String ans = "";
     private int count = 0;
-    public String getHappyString(int n, int k) {
-        List<String> allString = new ArrayList<>();
+    private char[] letters = new char[]{'a','b','c'};
+
+    public String getHappyString(int n, int k) { 
+
         ans = "";
         count = 0;
         bracktrack(new StringBuilder(),n, k);
-        
         return ans;
         
     }
 
     public void bracktrack( StringBuilder path,int n, int k){
-        if(count >= k ) return;
+        if(count == k ) return;
         if( path.length() == n ) {
             count++;
             if(count == k ){
                 ans = path.toString();
             }
             return;
-        }else if(path.length() > n) {
-            return;
         }
 
-        for(char c : new char[]{'a','b','c'}) {
+        for(char c : letters ) {
             
             if( path.length() < 1 ||
                 c != path.charAt(path.length()-1)
