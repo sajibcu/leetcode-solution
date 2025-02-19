4e76c8590ffad8cd0cbffae3222f901c33e9612c
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-19 15:00:12 +0800

    Time: 12 ms (57.63%) | Memory: 45 MB (39.18%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        List<String> allString = new ArrayList<>();
+        bracktrack(allString, new StringBuilder(),n);
+        String ans = "";
+        
+        return k > allString.size() ? "" : allString.get(k-1);
+        
+    }
+
+    public void bracktrack(List<String> s, StringBuilder path,int n){
+        if( path.length() == n ) {
+           // if("")
+            s.add(path.toString());
+            return;
+        }else if(path.length() > n) {
+            return;
+        }
+
+        for(char c : new char[]{'a','b','c'}) {
+            
+            if( path.length() < 1 ||
+                c != path.charAt(path.length()-1)
+            ) {
+                path.append(c);
+                bracktrack(s,path,n);
+                path.deleteCharAt(path.length() - 1);
+            }
+        }
+    }
+}
