d32d9bad54bf2b006cba9d3af6df23566f1334b6
Md. Sajib Hosen <0sajib0@gmail.com>
2025-02-23 18:07:08 +0800

    Time: 2 ms (7.59%) | Memory: 44.1 MB (6.25%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,8 +14,9 @@
  * }
  */
 class Solution {
-    int cont = 0;
-    public TreeNode constract( int[] preorder,int[] postorder, int l,int r,HashMap<Integer,Integer> preeMp,HashMap<Integer,Integer> postMp) {
+    HashMap<Integer,Integer> preeMp;
+    HashMap<Integer,Integer> postMp;
+    public TreeNode constract( int[] preorder,int[] postorder, int l,int r) {
         if (l > r) return null;
         int val = preorder[l];
         TreeNode root = new TreeNode(val);
@@ -28,8 +29,8 @@ class Solution {
         if (postIndex < 0 ) return root;
        
         int preIndex = preeMp.get(postorder[postIndex]);
-        root.left = constract(preorder,postorder,l+1, preIndex-1,preeMp,postMp);
-        root.right = constract(preorder,postorder,preIndex, r,preeMp,postMp);
+        root.left = constract(preorder,postorder,l+1, preIndex-1);
+        root.right = constract(preorder,postorder,preIndex, r);
 
         return root;
 
@@ -37,13 +38,13 @@ class Solution {
 
     }
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
-        HashMap<Integer,Integer> preeMp = new HashMap<>();
-        HashMap<Integer,Integer> postMp = new HashMap<>();
+        preeMp = new HashMap<>();
+        postMp = new HashMap<>();
         for(int i =0;i<postorder.length; i++){
             preeMp.put(preorder[i],i);
             postMp.put(postorder[i],i);
         }
-       return constract(preorder,postorder,0,preorder.length-1 ,preeMp,postMp);
+       return constract(preorder,postorder,0,preorder.length-1);
         
     }
 }
