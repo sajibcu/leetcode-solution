4af79b1bf5159c5058a5bf685b8fe24ab8abc4c7
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-02 10:09:46 +0800

    Time: 0 ms (100.00%) | Memory: 45 MB (84.13%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,37 +1,26 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
 
-        List<int[]> n1 = Arrays.stream(nums1)
-        .sorted( (a,b) -> Integer.compare(a[0],b[0]))
-        .toList();
-
-        List<int[]> n2 = Arrays.stream(nums2)
-        .sorted( (a,b) -> Integer.compare(a[0],b[0]))
-        .toList();
 
         List<int[]> ans = new ArrayList<>();
 
-        int i =0, j= 0, l1 = n1.size(), l2 = n2.size();
-        while( i < l1 || j< l2 ) {
-            if(i < l1 && j< l2) {
-                if( n1.get(i)[0] == n2.get(j)[0] ) {
-                    n1.get(i)[1] += n2.get(j)[1];
-                    ans.add(n1.get(i));
-                    i++;
-                    j++;
-                }else if( n1.get(i)[0] < n2.get(j)[0] ) {
-                    ans.add(n1.get(i++));
-                }else {
-                    ans.add(n2.get(j++));
-                }
-            }else if( i < l1) {
-                ans.add(n1.get(i++));
-            }else {
-                ans.add(n2.get(j++));
-            }
+        int i =0, j= 0, l1 = nums1.length, l2 = nums2.length;
+        while( i < l1 && j< l2 ) {
+             if( nums1[i][0] == nums2[j][0]) {
+                ans.add( new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]} );
+                i++;
+                j++;
+             }else if(nums1[i][0] < nums2[j][0] ) {
+                ans.add( nums1[i++] );
+             }else {
+                ans.add( nums2[j++]);
+             }
         }
 
+        // add remaing item
+        while( i < l1 ) ans.add( nums1[i++] );
+        while( j < l2 ) ans.add( nums2[j++]);
+
         return ans.toArray(new int[0][]);
-        
     }
 }
