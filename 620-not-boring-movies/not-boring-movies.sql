27a9faceb732a1df9417a00ff579c408bc3a9193
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-02 10:21:02 +0800

    Time: 868 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select *
-from Cinema c
-where c.id %2 = 1 and c.description <> 'boring' 
-order by c.rating DESC
+select id,movie,description,rating
+from Cinema
+where id %2 = 1 and description <> 'boring' 
+order by rating DESC
