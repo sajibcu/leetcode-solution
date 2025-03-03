0cb7ba9e98c0720a3209206867ce3ef8859aeccd
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-03 09:52:35 +0800

    Time: 5 ms (85.39%) | Memory: 68.2 MB (24.11%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         
-        List<Integer> smalls = new ArrayList<>();
-        List<Integer> greters = new ArrayList<>();
-        List<Integer> privots = new ArrayList<>();
+        int index = 0;
+        int[] ans = new int[nums.length];
 
         for(int n: nums ) {
-            if( n < pivot ) {
-               smalls.add(n); 
-            } else if( n > pivot ) {
-               greters.add(n); 
-            }
-            else {
-                privots.add(n);
-            }
+            if( n < pivot ) ans[index++] = n;
         }
-        smalls.addAll( privots);
-        smalls.addAll(greters);
-        return smalls.stream().mapToInt(Integer::intValue).toArray();
+        for(int n: nums ) {
+            if( n == pivot ) ans[index++] = n;
+        }
+
+        for(int n: nums ) {
+            if( n > pivot ) ans[index++] = n;
+        }
+
+        return ans;
     }
 }
