cb1b3ac317b553c1d1e727a21720c108b85550d1
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-04 09:57:33 +0800

    Time: 0 ms (100.00%) | Memory: 7.6 MB (91.08%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        int threePow = 4782969; // 3^14 = 4782969
+
+        while (threePow > 0) {
+            if (threePow <= n) {
+                n -= threePow;
+            }
+            threePow /= 3;
+        }
+
+        return n == 0;
+    }
+};
