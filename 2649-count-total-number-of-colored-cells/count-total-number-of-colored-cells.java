49db7cacb0244372fa06aaa80889fd5567454579
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-05 09:49:25 +0800

    Time: 0 ms (100.00%) | Memory: 40.4 MB (73.36%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,12 +1,13 @@
 class Solution {
     public long coloredCells(int n) {
-        long ans = 1;
-        int interval = 0;
-        while( n > 1) {
-           interval +=4;
-           ans += interval;
-           n--;
-        }
-        return ans;
+        // long ans = 1;
+        // int interval = 0;
+        // while( n > 1) {
+        //    interval +=4;
+        //    ans += interval;
+        //    n--;
+        // }
+        // return ans;
+        return 1 + (long) n * (n-1) * 2;
     }
 }
