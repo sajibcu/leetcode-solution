fe3db6758986a7bf7ac78dc4526e409a99a6e302
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-09 15:17:30 +0800

    Time: 1422 ms (29.22%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -3,7 +3,7 @@ with em as (
     select 
     e.reports_to,
     count(1) cnt,
-    sum(age) age
+    AVG(age) AS  age
     from Employees e
     where e.reports_to is not null
     group by e.reports_to
@@ -12,7 +12,7 @@ select
 em.reports_to as employee_id,
 e.name,
 em.cnt as reports_count,
-round( em.age / em.cnt ) as average_age 
+round( em.age ) as average_age 
 from em
 left join Employees e on e.employee_id = em.reports_to
 order by 1 asc;
