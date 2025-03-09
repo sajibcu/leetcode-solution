c07bf5574698d7c2d2237fc4204db166fb0f0681
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-09 20:52:57 +0800

    Time: 947 ms (59.39%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
-
-select e.employee_id , e.department_id
-from Employee e
-where e.primary_flag = 'Y' or e.employee_id in (
+with singleDep as (
     select employee_id from Employee ee 
-    where ee.employee_id = e.employee_id 
     group by ee.employee_id
     having count(ee.employee_id) = 1
 )
+select e.employee_id , e.department_id
+from Employee e
+left join singleDep using(employee_id)
+where e.primary_flag = 'Y' or singleDep.employee_id is not null
 
 
