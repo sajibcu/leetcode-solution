cb2343d1ac2cd1feffd755034b2fb541efbff42b
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-18 22:00:42 +0800

    Time: 3 ms (98.64%) | Memory: 55.9 MB (33.79%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,14 +1,12 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int[] bit = new int[32];
-
+    
         int ans = 0;
 
         for( int i =0; i< nums.length; i++ ) {
 
             int j = i;
             int mask = 0;
-            //clearBit(bit);
             while( j< nums.length && (mask & nums[j]) == 0 ) {
                 mask |=nums[j];
                 j++;
@@ -17,27 +15,5 @@ class Solution {
         }
 
         return ans;
-    }
-
-    public int setBit(int[] bit, int n ) {
-        int i = 0;
-
-        while( i < 32 ) {
-            if( ( n & (1<<i) ) != 0  ) {
-                if (bit[i] == 1) {
-                    return 1;  // Bit already set
-                } else {
-                    bit[i] = 1;  // Mark bit position as set
-                }
-            }
-            i++;
-        }
-        return 0;
-    }
-
-     public void clearBit(int[] bit) {
-        for (int i = 0; i < 32; i++) {
-            bit[i] = 0;
-        }
     }
 }
