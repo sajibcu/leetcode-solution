b7de3236a927ed67734d6d18a69bdb5c1b5502b3
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-19 09:47:31 +0800

    Time: 7 ms (66.53%) | Memory: 57.3 MB (74.95%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -5,12 +5,9 @@ class Solution {
         for(int i=0;i<l - 2;i++){
             if( nums[i] == 0 ) {
                 ans++;
-                nums[i]=1;
-                if( nums[i+1] == 0 ) nums[i+1] = 1;
-                else nums[i+1] = 0;
-
-                if( nums[i+2] == 0 ) nums[i+2] = 1;
-                else nums[i+2] = 0;
+                nums[i] ^=1;
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
             }
         }
 
