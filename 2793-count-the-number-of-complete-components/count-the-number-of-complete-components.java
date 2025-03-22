57790379bd7ab1a4cab79997085e5fa77a85a427
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-22 21:36:48 +0800

    Time: 9 ms (83.39%) | Memory: 45 MB (84.76%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -51,7 +51,7 @@ class Solution {
     }
 
     public int find(int i) {
-        if( parent[i] == i) return i;
-        return find(parent[i]);
+        if( parent[i] != i) parent[i] = find(parent[i]);
+        return parent[i];
     }
 }
