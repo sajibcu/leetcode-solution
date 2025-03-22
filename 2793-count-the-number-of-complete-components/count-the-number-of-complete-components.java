e438a4e2bcd61e388732d40a57c13d2aa7c97792
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-22 21:34:03 +0800

    Time: 7 ms (92.98%) | Memory: 45.2 MB (72.09%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,57 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+    public int countCompleteComponents(int n, int[][] edges) {
+        parent = new int[n];
+        rank = new int[n];
+        int[] numberOfEadge = new int[n];
+
+        for(int i =0; i<n; i++) parent[i] = i;
+
+        for(int e[]: edges) {
+            union(e[0], e[1]); 
+        }
+
+        int[] numberOfNode = new int[n];
+
+        Set<Integer> st = new HashSet<>();
+        for(int i=0;i<n;i++) {
+            numberOfNode[find(i)]++;
+            st.add(find(i));
+        }
+
+        for(int e[]: edges) {
+            // union(e[0], e[1]);  
+            numberOfEadge[find(e[0])]++;
+        }
+
+        int completed = 0;
+
+        for(int root : st) {
+            int expected = numberOfNode[root] * (numberOfNode[root] - 1)/2;
+            if( numberOfEadge[root] == expected ) completed++;
+        }
+
+        return completed;
+    }
+
+    public void union(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+
+        if( rootU == rootV) return;
+
+        if( rank[rootU] >= rank[rootV]) {
+            rank[rootU]++;
+            parent[rootV] = rootU;
+        }else {
+            rank[rootV]++;
+            parent[rootU] = rootV;
+        }
+    }
+
+    public int find(int i) {
+        if( parent[i] == i) return i;
+        return find(parent[i]);
+    }
+}
