dca6801d6c39739c86756c70c242cd9d1abc285f
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-26 15:18:56 +0800

    Time: 7 ms (99.50%) | Memory: 70.2 MB (76.38%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -3,27 +3,38 @@ class Solution {
 
         int m = grid.length, n = grid[0].length;
         int size = m * n;
-        int[] values = new int[size];
+        int[] freq = new int[10001];
+        List<Integer> values = new ArrayList<>();
 
         int i=0;
         for( int[] row : grid) {
             for( int num: row) {
-                values[i++] = num; 
+                freq[num]++;
+                if(freq[num]==1) values.add(num); 
             }
         }
-
-        Arrays.sort(values);
+        if (values.size() < 2) return 0;
         int ans = 0;
 
-        int median = values[size/2];
-
+        int median =findMedian(freq, size/2);
+        //System.out.println(median);
         for( int num: values) {
             int gap =  Math.abs(median - num); 
             if( gap % x != 0) return -1;
-            ans +=gap/x;
+           // System.out.println(freq[num]);
+            ans +=(gap/x)*freq[num];
         }
 
         return ans;
         
+    }
+
+    public int findMedian(int[] freq, int size) {
+        int cnt = 0;
+        for( int i =0 ; i< freq.length; i++) {
+            cnt += freq[i];
+            if( cnt > size ) return i;
+        }
+        return freq.length;
     }
 }
