b6442cf9bb498bcf38f6e7f3171a622cf4e21267
Md. Sajib Hosen <0sajib0@gmail.com>
2025-03-27 09:49:11 +0800

    Time: 29 ms (60.90%) | Memory: 62.2 MB (22.44%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,26 +1,28 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
         Map<Integer,Integer> mp = new HashMap<>();
-        for(int n: nums) {
-            mp.put(n, mp.getOrDefault(n,0)+1);
-        }
 
         int dominant = 0;
         int max = 0;
-        for(int key : mp.keySet()) {
-            if( mp.get(key) > max ) {
-                dominant = key;
-                max = mp.get(key);
+        for(int n: nums) {
+            int cnt =  mp.getOrDefault(n,0)+1;
+            mp.put(n,cnt);
+            if( cnt > max) {
+                dominant = n;
+                max = cnt;
             }
         }
 
-        int left = 0;
+        int left = 0,right = max;
 
         for( int i=0; i< nums.size(); i++) {
             int n = nums.get(i);
-            if( n == dominant ) left++;
-            int right = max - left;
-            if( left > (i+1)/2 && right > (nums.size()-i-1)/2 ) return i;
+            if( n == dominant ){
+                left++;
+                right--;
+                if( left > (i+1)/2 && right > (nums.size()-i-1)/2 ) return i;
+            } 
+            
         }
 
         return -1;
