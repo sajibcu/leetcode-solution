54068618a4cf9a67bb5719cc24420b42af50a2e1
Md. Sajib Hosen <0sajib0@gmail.com>
2025-04-08 09:17:00 +0800

    Time: 0 ms (100.00%) | Memory: 44.4 MB (61.29%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,26 +1,13 @@
 class Solution {
     public int minimumOperations(int[] nums) {
 
-        int[] freq = new int[101];
-        for( int n: nums) freq[n]++;
-        int i =0;
-        int l = nums.length;
-        int cnt = 0;
-        if( isDistinct(freq) ) return cnt;
-        while( i < l) {
-            cnt++;
-            if( i < l ) freq[nums[i++]]--;
-            if( i < l ) freq[nums[i++]]--;
-            if( i < l ) freq[nums[i++]]--;
-            if( isDistinct(freq) ) return cnt;
+        boolean[] seen = new boolean[101];
+        for( int i = nums.length-1; i>=0; i-- ) {
+            if( seen[nums[i]] ) return i/3+1;
+            seen[nums[i]] = true;
         }
 
-        return cnt;
+        return 0;
         
-    }
-
-    public boolean isDistinct(int[] freq) {
-        for(int i=1;i<=100;i++) if(freq[i] > 1) return false;
-        return true;
     }
 }
