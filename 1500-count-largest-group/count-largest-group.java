b5c900ebde2fec35d482e57975a23cd72ded75bf
Md. Sajib Hosen <0sajib0@gmail.com>
2025-04-23 11:22:46 +0800

    Time: 4 ms (77.06%) | Memory: 43.6 MB (8.60%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,4 +1,6 @@
 class Solution {
+    int[] dp = new int[10000];
+
     public int countLargestGroup(int n) {
         int[] freq = new int[37];
         while( n > 0) {
@@ -18,11 +20,16 @@ class Solution {
 
     public int getDigitSum(int n) {
         int sum = 0;
+        int original = n;
         while(n > 0) {
+            if( dp[n] != 0 ) {
+                sum +=dp[n];
+                break;
+            }
             sum +=n%10;
             n /=10;
         }
-
+        dp[original] = sum;
         return sum;
     }
 }
