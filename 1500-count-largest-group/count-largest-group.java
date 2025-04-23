e308cbd3ef4f1a75855792ed3faea3e5a43d7fca
Md. Sajib Hosen <0sajib0@gmail.com>
2025-04-23 11:27:42 +0800

    Time: 4 ms (77.06%) | Memory: 43.6 MB (8.60%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -9,7 +9,6 @@ class Solution {
         int ans = 0;
         int mx = 0;
         for( int a: freq)  {
-            //System.out.println(a);
             if( a > ans) {
                 ans = a;
                 mx = 1;
@@ -19,17 +18,8 @@ class Solution {
     }
 
     public int getDigitSum(int n) {
-        int sum = 0;
-        int original = n;
-        while(n > 0) {
-            if( dp[n] != 0 ) {
-                sum +=dp[n];
-                break;
-            }
-            sum +=n%10;
-            n /=10;
-        }
-        dp[original] = sum;
-        return sum;
+        if( dp[n] != 0) return dp[n];
+        if( n < 10) return dp[n] = n;
+        return dp[n] = getDigitSum(n/10) + n%10;
     }
 }
