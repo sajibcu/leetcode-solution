2079855b45c5be38f3bcdec4f24d9a9a98da928d
Md. Sajib Hosen <0sajib0@gmail.com>
2025-07-01 13:28:29 +0800

    Time: 0 ms (100.00%) | Memory: 42.2 MB (61.16%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -3,19 +3,13 @@ class Solution {
         if (word == null || word.isEmpty()) return 0;
 
         int ans = 1;
-        int temp = 1;
         int i= 1;
-        char c = word.charAt(0);
         while(  i < word.length()) {
-            if( word.charAt(i) != word.charAt(i-1)) {
-                ans +=temp-1;
-                temp = 0;
+            if( word.charAt(i) == word.charAt(i-1)) {
+                ans ++;
             }
-            temp++;
             i++;
         }
-        ans +=temp-1;
-
         return ans;
         
     }
