8aab5633c5a5c16d35e5fdf1f16b144a9b984567
Md. Sajib Hosen <0sajib0@gmail.com>
2025-07-07 10:37:33 +0800

    Time: 34 ms (98.25%) | Memory: 73.1 MB (99.88%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,31 +1,26 @@
 class Solution {
+    int[] parent;
     public int maxEvents(int[][] events) {
 
         Arrays.sort(events, (a,b) -> a[1] - b[1]);
-        int max = 0,min=100000;
-        for(int[] e : events) {
-            max = Math.max(max,e[1]);
-            min = Math.min(min, e[0]);
-        }
-        TreeSet<Integer> availableDays = new TreeSet<>();
-
-        while(min <= max) 
-        {
-            availableDays.add(min);
-            min++;
-        }
+        parent = new int[100002];
+        for(int i=0;i<100002; i++) parent[i] = i;
         int ans = 0;
 
         for(int[] e : events) {
-            Integer day = availableDays.ceiling(e[0]);
-            if( day != null && day <=e[1] ) {
-                ans++;
-                availableDays.remove(day);
-            }
+           int day = find(e[0]);
+           if( day <= e[1]) {
+            ans++;
+            parent[day] = find(day+1);
+           }
         }
-        return ans;
+        return ans; 
+    }
 
-
-        
+    public int find(int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
     }
 }
