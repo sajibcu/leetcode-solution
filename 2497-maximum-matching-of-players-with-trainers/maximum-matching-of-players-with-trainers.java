576c7883b2032600dd57e9024d7076d1b251c5df
Md. Sajib Hosen <0sajib0@gmail.com>
2025-07-13 10:14:35 +0800

    Time: 5 ms (100.00%) | Memory: 64.9 MB (34.15%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,7 +1,20 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players);
-        Arrays.sort(trainers);
+        
+        Thread sortPlayers = new Thread(()-> Arrays.sort(players) );
+        Thread sortTrainers = new Thread(() -> Arrays.sort(trainers));
+        sortPlayers.start();
+        sortTrainers.start();
+        try{
+            sortPlayers.join();
+            sortTrainers.join();
+
+        }catch (Exception e) {
+            
+        }
+
+
+        // process
         int i=0,j=0;
         int pl = players.length;
         int tl = trainers.length;
