8b4a9580407996b4b01b10f15820594a61fefbf7
Md. Sajib Hosen <0sajib0@gmail.com>
2025-07-30 11:42:11 +0800

    Time: 4 ms (22.41%) | Memory: 61.2 MB (21.99%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -6,7 +6,9 @@ class Solution {
         int ans = 1;
         int temp = 1;
         for(int i=1;i<nums.length;i++) {
-            if( nums[i] == nums[i-1] && nums[i] == max) {
+            if(nums[i] != max) continue;
+
+            if( nums[i] == nums[i-1]) {
                 temp ++;
             }else temp = 1;
 
