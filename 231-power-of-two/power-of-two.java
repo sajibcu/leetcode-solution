97785a0a508a7466135c6930634ed3d5ea4dbc4b
Md. Sajib Hosen <0sajib0@gmail.com>
2025-08-09 08:25:24 +0800

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.12%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <=0) return false;
+
+        for(int i = 0 ; i< 32; i++) {
+            if( (1<<i) == n) return true;
+        }
+        return false;
+        
+    }
+}
