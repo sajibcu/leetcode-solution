956bf509acb592e36ce487f23c08e226891fdd83
Md. Sajib Hosen <0sajib0@gmail.com>
2025-08-10 09:36:28 +0800

    Time: 0 ms (100.00%) | Memory: 41 MB (47.37%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,16 +1,44 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-
-        List<String> all = new ArrayList<>();
-        String s = String.valueOf(n);
-        permute(s.toCharArray(),0,all);
-        for(String num : all) {
-            if( num.charAt(0)=='0') continue;
-            if(isPowerOf2(Integer.parseInt(num))) return true;
-
+        int[] precomputed = new int[32];
+        for(int i=0;i<32;i++){
+            precomputed[i] = (1<<i);
         }
+        int[] digitFrequency = getDigitFrequency(n);
+        for(int i=0;i<32;i++){
+            int[] freq = getDigitFrequency((1<<i));
+            if(equal(digitFrequency,freq)) return true;
+        }
+
         return false;
     }
+    public boolean equal(int[] a, int[] b) {
+        for(int i=0;i<a.length;i++) {
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
+    public int[] getDigitFrequency(int n) {
+        int[] digitFrequency = new int[10];
+        while(n > 0) {
+            digitFrequency[n%10]++;
+            n/=10;
+        }
+        return digitFrequency;
+    }
+
+    // public boolean reorderedPowerOf2(int n) {
+
+    //     List<String> all = new ArrayList<>();
+    //     String s = String.valueOf(n);
+    //     permute(s.toCharArray(),0,all);
+    //     for(String num : all) {
+    //         if( num.charAt(0)=='0') continue;
+    //         if(isPowerOf2(Integer.parseInt(num))) return true;
+
+    //     }
+    //     return false;
+    // }
 
     public void permute(char[] arr,int index, List<String> result) {
         if( index == arr.length) {
