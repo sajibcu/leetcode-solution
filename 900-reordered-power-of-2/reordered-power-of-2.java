9115bdd48a29eb55112f1a18e40f33e7a9a32fb6
Md. Sajib Hosen <0sajib0@gmail.com>
2025-08-10 09:44:17 +0800

    Time: 0 ms (100.00%) | Memory: 40.9 MB (55.26%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        int l = String.valueOf(n).length();
 
         int[] digitFrequency = getDigitFrequency(n);
-        for(int i=0;i<32;i++){
-            int powerOf2= (1<<i);
-            if(l != String.valueOf(powerOf2).length() ) continue; // avoid unnecesary check
-            int[] freq = getDigitFrequency(powerOf2);
+        for(int i=0;i<31;i++){
+            int[] freq = getDigitFrequency((1<<i));
             if(equal(digitFrequency,freq)) return true;
         }
 
