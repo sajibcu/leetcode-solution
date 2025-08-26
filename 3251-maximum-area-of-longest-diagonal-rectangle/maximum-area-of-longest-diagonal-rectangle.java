2bcfae2bfc5c0f3e2579c987f8cd18558b2f3156
Md. Sajib Hosen <0sajib0@gmail.com>
2025-08-26 14:10:36 +0800

    Time: 1 ms (96.00%) | Memory: 44.5 MB (62.77%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -1,18 +1,31 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] dimensions) {
 
-        double diagonal = 0f;
+        long diagonal = 0L;
         int area = 0;
 
         for(int d[] : dimensions ) {
-            double current = Math.sqrt(d[0]*d[0] + d[1]*d[1]);
+            long current = d[0]*d[0] + d[1]*d[1];
             if( current > diagonal) {
                 diagonal = current;
                 area = d[0]*d[1];
             }else if( current ==diagonal ) area = Math.max(area, d[0]*d[1]);
         }
-
-        return area;
-        
+        return area;   
     }
+
+    // public int areaOfMaxDiagonal(int[][] dimensions) {
+
+    //     double diagonal = 0f;
+    //     int area = 0;
+
+    //     for(int d[] : dimensions ) {
+    //         double current = Math.sqrt(d[0]*d[0] + d[1]*d[1]);
+    //         if( current > diagonal) {
+    //             diagonal = current;
+    //             area = d[0]*d[1];
+    //         }else if( current ==diagonal ) area = Math.max(area, d[0]*d[1]);
+    //     }
+    //     return area;   
+    // }
 }
