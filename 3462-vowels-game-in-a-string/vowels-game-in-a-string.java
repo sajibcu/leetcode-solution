a2792e93469aaf6563b59c1153db1d1e56048003
Md. Sajib Hosen <0sajib0@gmail.com>
2025-09-12 10:25:37 +0800

    Time: 11 ms (34.12%) | Memory: 45.8 MB (16.80%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -2,11 +2,9 @@ class Solution {
     public boolean doesAliceWin(String s) {
         int vowels = 0;
         for(int i =0; i< s.length(); i++ ) {
-            if( isVowel(s.charAt(i))) vowels++;
+            if( isVowel(s.charAt(i))) return true;
         }
-
-        if( vowels ==0  ) return false;
-        return true;
+        return false;
     }
 
     public boolean isVowel( char c) {
