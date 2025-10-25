d8c817bf4c78318c3252fbd2db07d2a774059f9d
Md. Sajib Hosen <0sajib0@gmail.com>
2025-10-25 22:43:50 +0800

    Time: 0 ms (100.00%) | Memory: 40.7 MB (32.47%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,5 +1,13 @@
 class Solution {
     public int totalMoney(int n) {
+        int weeks = n / 7;
+        int days = n % 7;
+        int totalWeeks = 7 * weeks * (weeks + 7) / 2;
+        int remaining = days * (weeks + 1) + days * (days - 1) / 2;
+        return totalWeeks + remaining;
+    }
+
+    public int totalMoney1(int n) {
 
         int w = 1;
         int c = 1;
