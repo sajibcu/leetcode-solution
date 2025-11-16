98f3e56e9ccce1b044a331e6cbd2ec32ad762b53
Md. Sajib Hosen <0sajib0@gmail.com>
2025-11-16 10:12:50 +0800

    Time: 5 ms (57.07%) | Memory: 46.5 MB (6.02%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -5,9 +5,12 @@ class Solution {
         long ans = 0;
         long one = 0;
         for(int i=0;i<s.length(); i++) {
-            if( s.charAt(i) == '1') one++;
+            if( s.charAt(i) == '1'){
+                one++;
+                ans =(one+ans)%mod; 
+            }
             else one = 0;
-            ans =(one+ans)%mod; 
+            
         }
 
         return (int)ans;
